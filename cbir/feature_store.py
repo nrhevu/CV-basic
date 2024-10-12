8e28f695edc508eee68609f56ca6d27b40d72daf
Vu0811 <thevuu2001@gmail.com>
2024-10-12 11:59:34 +0700

    fix: type error

--- cbir/feature_store.py
+++ cbir/feature_store.py
@@ -11,23 +11,20 @@ from cbir.entities.search_objects import ImageSearchObject
 class FeatureStore(ABC):
     def check_input_index(self, images: np.ndarray | list, features: np.ndarray | list):
         if isinstance(images, list) and isinstance(features, list):
-            assert len(images) == len(
-                features
-            ), "Images and features must have same length"
-            assert (
-                len(images) > 0 and len(features) > 0
-            ), "Images and features cannot be empty"
-            assert isinstance(images[0], np.ndarray) and isinstance(
-                features[0], np.ndarray
-            ), "Images and features must be both np.ndarray or list of np.ndarray"
+            assert len(images) == len(features), \
+                "Images and features must have same length"
+            assert (len(images) > 0 and len(features) > 0), \
+                "Images and features cannot be empty"
+            # assert isinstance(images[0], np.ndarray) and isinstance(features[0], np.ndarray), \
+            #     "Images and features must be both np.ndarray or list of np.ndarray"
         elif isinstance(images, np.ndarray) and isinstance(features, np.ndarray):
             images = [images]
             features = [features]
-        else:
-            raise TypeError(
-                f"Invalid type for images {type(images)} and features {type(features)} \
-                must be both np.ndarray or list of np.ndarray"
-            )
+        # else:
+        #     raise TypeError(
+        #         f"Invalid type for images {type(images)} and features {type(features)} \
+        #         must be both np.ndarray or list of np.ndarray"
+        #     )
 
         return images, features
     
