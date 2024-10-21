ba4c6921cc04723fd52993c60232599e92a6c671
nrhevu <thevuu2001@gmail.com>
2024-10-22 00:57:07 +0700

    update sh

--- evaluation.sh
+++ evaluation.sh
@@ -1,5 +1,8 @@
-# TQDM_DISABLE=1 python histogram_knn_eval.py
-# TQDM_DISABLE=1 python sift_knn_eval.py
-# TQDM_DISABLE=1 python resnet_knn_gpu_eval.py
-# TQDM_DISABLE=1 python ensemble_sift_color.py
+TQDM_DISABLE=1 python histogram_knn_eval.py
+TQDM_DISABLE=1 python edge_histogram_knn_eval.py
+TQDM_DISABLE=1 python fourier_descriptor_knn_eval.py
+TQDM_DISABLE=1 python color_edge_fourier_eval.py
+TQDM_DISABLE=1 python sift_knn_eval.py
+TQDM_DISABLE=1 python resnet_knn_gpu_eval.py
+TQDM_DISABLE=1 python ensemble_sift_color.py
 TQDM_DISABLE=1 python ensemble_all.py
